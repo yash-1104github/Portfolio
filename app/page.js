5a3889ca4b5d5636f6f6e294fb8a3738a84e6e69
yashguptamd <yash.gupta@materialdepot.in>
2025-07-16 04:14:05 +0530

    update

--- app/page.js
+++ app/page.js
@@ -166,7 +166,7 @@ export default function Home() {
               </li>
             </Link>
             <a
-              href="https://drive.google.com/file/d/1NtoVdCu-BeQBbCimQh1wz_BmND4ZmCkG/view?usp=sharing"
+              href="https://drive.google.com/file/d/1rl3Py3tmiPsy35LGINg22jxXr6nYyx8_/view?usp=sharing"
               target="_blank"
             >
               <li className="rounded-full bg-white text-black cursor-pointer px-3  py-2 font-bold hover:text-sky-900">
